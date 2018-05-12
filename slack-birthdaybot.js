f6e6f2d07310443b6fba6e354736227cac33b357
naotowatari-github <39210146+naotowatari-github@users.noreply.github.com>
2018-05-12 15:33:01 +0900

    Add files via upload

---
+++ slack-birthdaybot.js
@@ -0,0 +1,44 @@
+// 実行したいスクリプト本体
+function main() {
+  var objSpreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var objSheet = objSpreadsheet.getActiveSheet();
+  var objCell = objSheet.getActiveCell();
+   
+  var today = Utilities.formatDate(new Date(), "Asia/Tokyo", "MM-dd");
+
+  var data = objSheet.getRange(2, 3, objSheet.getLastRow() - 1).getValues();
+
+  var array = [];
+
+　for(var i=0; i<data.length; i++){
+    if(Utilities.formatDate(data[i][0], "Asia/Tokyo", "MM-dd") == today){
+        var happyman = objSheet.getRange(i+2, 2).getValue();
+        array.push(happyman);
+        Logger.log(array);
+        
+          var postUrl = 'https://hooks.slack.com/services/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';
+          var username = 'Happy Birthday';  // 通知時に表示されるユーザー名
+          var icon = ':birthday:';  // 通知時に表示されるアイコン
+          var message = '今日は' + array + 'の誕生日です。おめでとうございます！';  // 投稿メッセージ
+          }
+  }
+  
+  if (array != ""){
+    var jsonData =
+        {
+          "username" : username,
+          "icon_emoji": icon,
+          "text" : message
+        };
+    var payload = JSON.stringify(jsonData);
+    
+    var options =
+        {
+          "method" : "post",
+          "contentType" : "application/json",
+          "payload" : payload
+        };
+    
+    UrlFetchApp.fetch(postUrl, options);
+  }
+}
